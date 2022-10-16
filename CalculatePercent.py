89d73ebba3b2f07b21515508d6e4f98af4f2dfe2
hmohammadi98 <115883734+hmohammadi98@users.noreply.github.com>
2022-10-16 18:24:32 +0330

    Create CalculatePercent.py

---
+++ CalculatePercent.py
@@ -0,0 +1,8 @@
+"""next project use function
+calculate percent
+for example 0.2 ==>20%"""
+def percent(x):
+    x1=x*100
+    print("number is percent",x1,"%",)
+number=float(input("please enter number:"))
+percent(number)
